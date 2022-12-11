1a3c2d53f49f7f7213e6d5b86c0c39fa0da9b593
kimura.shinnosuke <shiinosuke.kimura79@gmail.com>
2022-12-11 23:10:40 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 布団 on 2022/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
